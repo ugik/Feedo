97fff508bca5f8e22d1be05785ab83d248b65897
ugik <georgek@gmail.com>
2011-11-16 18:51:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedo::Application.config.secret_token = 'e5e087f57c47b3038b6e14f106c150381e10105baae469d4ff927ca7a8e40603f952e0f104505724749f17c585ca988d4333b94ce9aaff6f9390aa4bbe74de4b'
