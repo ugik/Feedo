74ec0de3e249a7e0173421ad801b8b576f1a1aa2
Ethan Klein <ethan@niceness.org>
2011-12-06 11:58:09 -0800

    Attempting to bust the asset cache so new styles will be seen

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,6 @@ module Feedo
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
