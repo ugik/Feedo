2a1f453b097772be65c30c478ce33533619f78c5
Ethan Klein <ethan@niceness.org>
2011-12-08 11:37:19 -0800

    Fixing bug with nil class when calling avatar_file_name

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,24 +2,24 @@ class PostsController < ApplicationController
   respond_to :html, :json
 
   def index
-    # TODO - Reimplement this stuff
-    if params[:delay] != nil
-      # %meta{:content => params[:delay], "http-equiv" => "REFRESH"}/
-    end
-
-    if params[:font_name] != nil
-      # @font_name = params[:font_name]
-    else
-      # @font_name = 'ubuntu' # default font
-    end
-
-    if params[:random_image] != nil
-      # @post = Post.random_image
-    end
-
-    if params[:random] != nil
-      # @post = Post.random
-    end
+    # TODO - Reimplement these query-string-parameter controls
+    #if params[:delay] != nil
+    #  %meta{:content => params[:delay], "http-equiv" => "REFRESH"}/
+    #end
+    #
+    #if params[:font_name] != nil
+    #  @font_name = params[:font_name]
+    #else
+    #  @font_name = 'ubuntu' # default font
+    #end
+    #
+    #if params[:random_image] != nil
+    #  @post = Post.random_image
+    #end
+    #
+    #if params[:random] != nil
+    #  @post = Post.random
+    #end
     @post = Post.where(:postable_type => "League").random
   end
 
